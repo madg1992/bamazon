03319f9909b2397c20803b251a9dca3fc04edb94
Mark Gallardo <mrk_gllrd@yahoo.com>
2019-11-30 21:55:47 -0600

    added a function to ask user what product they would like to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,18 +5,71 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "madg1992",
     database: "bamazon_DB"
 });
 
-// connection.connect(function(err){
-//     if (err) throw err;
-//     console.log("Connected as id: "+connection.threadId);
-// })
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    //test whether the connection works
+    console.log("Connected as id: " + connection.threadId);
+    start();
+})
 
 var start = function () {
-    connection.query('SELECT * FROM PRODUCTS', function (err, res) {
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
-        //console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                res[i].item_id + " " +
+                res[i].product_name + " " +
+                res[i].department_name + " " +
+                res[i].price + " " +
+                res[i].stock_quantity + "\n"
+            );
+        }
+        askQuestion(res);
     })
 }
+
+//need a function that ask the user to select one of these options and purchase it
+var askQuestion = function (res) {
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "Please type in the product name you would like to order:"
+    }]).then(function (answer) {
+        var correct = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quantity",
+                    message: "How many of this item woyld you like to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function (answer) {
+                    if ((res[id].stock_quantity - answer.quantity) > 0) {
+                        connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.quantity) + "' WHERE product_name'" + product + "'", function (err, response) {
+                            console.log("You successfully bought the product!");
+                            start();
+                        })
+                    } else {
+                        console.log("Not a valid selection!");
+                        askQuestion(res);
+                    }
+                })
+            }
+        }
+    })
+}
+
---
+++ bamazonProducts.sql
@@ -0,0 +1,13 @@
+INSERT INTO products
+    (product_name,department_name,price,stock_quantity)
+VALUES
+    ("Cetaphil Cleanser", "Beauty", 9.95, 100),
+    ("Organic Honey", "Food", 15.98, 110),
+    ("Beats Solo3 Headphones", "Electronics", 129.99, 100),
+    ("Fire HD 8 Tablet", "Electronics", 49.99, 100),
+    ("JBL Bluetooth Speaker", "Electronics", 99.00, 100),
+    ("Lindt Chocolate Truffles", "Food", 15.97, 200),
+    ("Trident Gum 12 Pack", "Food", 7.90, 150),
+    ("Tatcha Face Cream", "Beauty", 68.00, 100),
+    ("Dermalogica Eye Cream", "Beauty", 65.00, 100),
+    ("Pokemon Sword", "Video Game", 59.99, 200);
